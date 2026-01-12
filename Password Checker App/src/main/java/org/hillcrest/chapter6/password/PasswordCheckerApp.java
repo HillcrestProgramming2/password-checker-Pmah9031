81afc5f706edce2a1800547b3e8fe02902f90ccb
PrakashMahanthi <prakash.mahanthi12@gmail.com>
2026-01-12 09:32:51 -0700

    Update PasswordCheckerApp.java

--- Password Checker App/src/main/java/org/hillcrest/chapter6/password/PasswordCheckerApp.java
+++ Password Checker App/src/main/java/org/hillcrest/chapter6/password/PasswordCheckerApp.java
@@ -1,8 +1,40 @@
 package org.hillcrest.chapter6.password;
 
-public class PasswordCheckerApp {
-    //main method goes here
-    //prompt user for password
-    //Check strength
-    //Provide user feedback based on the strength
+
+import java.util.Scanner;
+
+
+public class PasswordCheckerApp
+{
+   public static void main(String[] args)
+   {
+       Scanner sc = new Scanner(System.in);
+
+
+       System.out.print("Enter a password: ");
+       String password = sc.nextLine();
+
+
+       int score = CriteriaChecker.evaluateCriteria(password);
+       String strength = CriteriaChecker.determineStrength(score);
+
+
+       String feedback = FeedbackGenerator.generateFeedback(password);
+
+
+       // Print strength + score
+       System.out.println();
+       System.out.println("Password Strength: " + strength + " (" + score + "/5)");
+
+
+       // Only print suggestions if feedback is not empty AND not strong
+       if (score <=1) {
+           System.out.println("Suggestions to improve your password:");
+           System.out.print(feedback);
+       }
+
+
+
+
+   }
 }
